1779c703197b76b94dff42d70aafb37db3092df0
CUI Hao <cuihao.leo@gmail.com>
2015-01-02 19:49:56 +0800

    优化：仅当文件更改时重载文件列表

--- movie.js
+++ movie.js
@@ -1,9 +1,13 @@
 var douban_movie = require("./lib/douban_movie.js");
 var parse_file_list = require("./lib/parse_file_list");
 var ReadWriteLock = require('rwlock');
+var fs = require('fs');
 
 var express = require('express');
 var app = express();
+
+var filelist_mtime = new Date(0);
+var filelist = [];
 
 app.use(express.static(__dirname + '/static'));
 
@@ -15,47 +19,68 @@ function fillMovieInfo (obj, minfo) {
     obj.aka = minfo.aka;
 }
 
-app.get('/list.json', function (req, res) {
-    try {
-        var json = require("./list.json");
-    } catch (err) {
-        console.error("list.json not found!");
-        res.json({});
-        return;
-    };
+function loadFileList () {
+    fs.stat("list.json", function (err, stats) {
+        if (err) {
+            console.error("Cannot read list.json !");
+            return;
+        }
 
-    var ret = new Object();
-    var cb_lock = new ReadWriteLock();
+        if (stats.mtime - filelist_mtime == 0) {
+            return;
+        }
 
-    parse_file_list.getMovieFiles(json, [], function (err, fpath, finfo) {
-        cb_lock.readLock ("L1", function (release) {
+        try {
+            var li = require('./list.json');
+        } catch (e) {
+            console.error("Cannot read list.json!");
+            return;
+        }
+
+        var tmp = [];
+        parse_file_list.getMovieFiles(li, [], function (err, p, info) {
             if (err) {
                 throw err;
             }
-            else {
-                parse_file_list.getMID(fpath, function (err, mid) {
-                    if (err) {
-                        mid = -1;
-                    }
-                    
-                    var fi = {
-                        path: fpath,
-                        size: finfo.size,
-                        time: finfo.time
-                    };
 
-                    if (ret[mid]) {
-                        ret[mid].files.push(fi);
-                    }
-                    else {
-                        ret[mid] = new Object();
-                        ret[mid].files = [fi];
-                    }
+            tmp.push([p, info]);
+        });
 
-                    release();
-                });
-            }
-        })
+        console.log("list.json reloaded!");
+        filelist_mtime = stats.mtime;
+        filelist = tmp;
+    });
+}
+
+app.get('/list.json', function (req, res) {
+    var start_time = (new Date()).getTime();
+    var ret = new Object();
+    var cb_lock = new ReadWriteLock();
+
+    process.nextTick(loadFileList);
+
+    filelist.forEach(function (elem) {
+        cb_lock.readLock ("L1", function (release) {
+            var fpath = elem[0], finfo = elem[1];
+
+            parse_file_list.getMID(fpath, function (err, mid) {
+                if (err) {
+                    return;
+                }
+                
+                var fi = {path: fpath, size: finfo.size, time: finfo.time};
+
+                if (ret[mid]) {
+                    ret[mid].files.push(fi);
+                }
+                else {
+                    ret[mid] = new Object();
+                    ret[mid].files = [fi];
+                }
+
+                release();
+            });
+        });
     });
 
     cb_lock.writeLock("L1", function (release) {
@@ -87,18 +112,28 @@ app.get('/list.json', function (req, res) {
 
         cb_lock.writeLock("L2", function (release) {
             res.json(ret);
+            var end_time = (new Date()).getTime();
+            console.log("Response time:", (end_time - start_time)/1000);
+            last_ret = ret;
             release();
         });
 
         release();
     });
-
 });
 
-var port = Number(process.argv[2]);
-if (!(port >= 0 && port < 65536)) {
-    port = 3000;
+
+if (require.main === module) {
+
+    var port = Number(process.argv[2]);
+
+    if (!(port >= 0 && port < 65536)) {
+        port = 3000;
+    }
+
+    loadFileList();
+
+    console.log("Listen at port " + port);
+    app.listen(port);
 }
 
-console.log("Listen at port " + port);
-app.listen(port);
